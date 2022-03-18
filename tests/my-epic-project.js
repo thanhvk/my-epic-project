59009ea25b4ae538930fbc49a1e4faadb9a4573a
thanhvk <thanhvk.w3s@gmail.com>
2022-03-18 15:24:10 +0700

    complete section 2

--- tests/my-epic-project.js
+++ tests/my-epic-project.js
@@ -1,13 +1,40 @@
 const anchor = require('@project-serum/anchor');
+const { SystemProgram } = anchor.web3;
 
 const main = async() => {
   console.log("🚀 Starting test...")
 
-  anchor.setProvider(anchor.Provider.env());
+  const provider = anchor.Provider.env();
+  anchor.setProvider(provider);
+
   const program = anchor.workspace.MyEpicProject;
-  const tx = await program.rpc.startStuffOff();
+  const baseAccount = anchor.web3.Keypair.generate();
+
+  let tx = await program.rpc.startStuffOff({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId,
+    },
+    signers: [baseAccount],
+  });
 
   console.log("📝 Your transaction signature", tx);
+
+  let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log('👀 GIF Count', account.totalGifs.toString());
+
+  await program.rpc.addGif("https://media3.giphy.com/media/qSWM4R1wUvAZ6wGsgu/giphy.gif?cid=ecf05e477cd20b7dc83934e35c3f72749de9ae4ec9f71c77&rid=giphy.gif&ct=g", {
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+    },
+  });
+  
+  account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log('👀 GIF Count', account.totalGifs.toString());
+
+  console.log('👀 GIF List', account.gifList)
 }
 
 const runMain = async () => {
